a35225054b162d6d68d8e9925fb2acd271abdd64
jacobogart <jacobogart@gmail.com>
2019-07-15 16:54:56 -0600

    Add basic ShowScreen styling

--- components/ResultCard.js
+++ components/ResultCard.js
@@ -3,7 +3,7 @@ import { TouchableHighlight, Text, View, ImageBackground, StyleSheet } from 'rea
 import { cleanDate, cleanTime } from './helper';
 
 export default function ResultCard(props) {
-  const { id, name, venue_name } = props.data;
+  const { id, name, venue_name, poster_url } = props.data;
   const { resultText, card, header, textHolder } = localStyles;
   const date = cleanDate(props.data.date);
   const time = cleanTime(props.data.date);
@@ -13,7 +13,7 @@ export default function ResultCard(props) {
       onPress={() => props.navigate("Show", { id })}
     >
       <ImageBackground
-        source={{ uri: props.data.poster_url }}
+        source={{ uri: poster_url }}
         style={{ width: "100%", height: "100%", opacity: 0.9 }}
         blurRadius={7}
       >
