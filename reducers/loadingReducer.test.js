8f3d504b51659046487c05484e88247e3ef4d94c
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-15 20:53:48 -0600

    Add loadingReducer test

--- reducers/loadingReducer.test.js
+++ reducers/loadingReducer.test.js
@@ -1,6 +1,21 @@
+ import { loadingReducer } from './loadingReducer';
+ import * as actions from '../actions';
 
-describe('loadingReducer', () => {
-  it('should pass a test', () => {
-    expect(true).toBe(true);
-  });
-});
+ describe ('isLoadingReducer', () => {
+
+    it('should return default state', () => {
+      const expected = false;
+      const result = loadingReducer(undefined, {});
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should change is loading on the state tree', () => {
+      const bool = true;
+      const action = actions.isLoading(bool);
+  
+      const result = loadingReducer(undefined, action);
+   
+      expect(result).toEqual(true);
+    });
+ });
