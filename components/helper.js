b64e5cc0435a0a941d302b06965e6ecf8729ab8c
jacobogart <jacobogart@gmail.com>
2019-07-17 11:08:23 -0600

    Add AddShowForm functionality and styling

--- components/helper.js
+++ components/helper.js
@@ -12,10 +12,20 @@ export const cleanTime = dateObj => {
   const formattedTime = date
     .parse(`${dateObj.split(" ")[1].split(".")[0]}`, "hh:mm:ss");
   let cleanTime = date.format(formattedTime, "hh:mm A");
-  if (cleanTime[0]) {
+  if (cleanTime[0] === '0') {
     let timeParts = cleanTime.split("");
     timeParts.shift();
     cleanTime = timeParts.join('');
   }
   return cleanTime;
 };
+
+export const cleanTimeJS = (dateObj) => {
+  let cleanTime = date.format(dateObj, "hh:mm A");
+  if (cleanTime[0] === "0") {
+    let timeParts = cleanTime.split("");
+    timeParts.shift();
+    cleanTime = timeParts.join("");
+  }
+  return cleanTime
+}
