3c51413f4f5e21d274fb2415af586718f434fa05
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-19 10:50:59 -0600

    Add several tests to fetch shows. In progress to get them passed

--- redux/thunks/__tests__/fetchShows.js
+++ redux/thunks/__tests__/fetchShows.js
@@ -8,11 +8,39 @@ describe("fetchShows Thunk", () => {
   beforeEach(() => {
     mockDispatch = jest.fn();
     mockUrl = "www.dragme.com";
+    window.fetch = jest.fn().mockImplementation(() => 
+        Promise.resolve({
+            json: () => Promise.resolve(mockShows),
+            ok:true
+        })  
+      )
   });
 
   it("should call dispatch with isLoading(true)", () => {
-    const thunk = fetchShows(mockUrl, getShows, "GET");
+    let thunk = fetchShows(mockUrl, getShows, "GET");
     thunk(mockDispatch);
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
   });
+  
+  it("should dispatch setError with a message if the response is not okay ", async() => {
+      window.fetch=jest.fn().mockImplementation(()=> 
+          Promise.resolve({
+              ok:false,
+              statusText: "Error has occured"
+          })
+      )
+      let thunk = await fetchShows(mockUrl, getShows, "GET", isLoading(true));
+      await thunk(mockDispatch);
+      expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
+  });
+
+  it.skip("should dispatch saveShows", async () => {
+      let mockShows = {name: "Drag Show", date: "Tomorrow", description: "Best Drag Show ever", event_url: 'URL', poster_url: "URL"}
+      const thunk = await fetchShows(mockUrl, getShows, "GET");
+      await thunk(mockDispatch);
+
+      expect(mockDispatch).toHaveBeenCalledWith(fetchShows(mockShows));
+  })
+
+
 });
