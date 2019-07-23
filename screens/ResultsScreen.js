82a433feac2b13ac708785e0b3f635bc622f04e0
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-23 13:27:52 -0600

    Begin search bar functionalitly

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ScrollView, StyleSheet, ActivityIndicator } from "react-native";
+import { ScrollView, StyleSheet, ActivityIndicator, View, TextInput } from "react-native";
 import ResultCard from "../components/ResultCard";
 import { mainStyles } from "../constants/mainStyles";
 import { primaryColor, accentColor, secondaryColor } from "../constants/Colors";
@@ -11,6 +11,7 @@ import { fetchVenues } from "../redux/thunks/fetchVenues";
 export class ResultsScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = { input: '' };
   }
 
   componentDidMount() {
@@ -18,22 +19,54 @@ export class ResultsScreen extends Component {
     this.props.fetchVenues();
   }
 
+  handleTextChange = (text) => {
+    this.setState({input: text})
+    this.filterResults(text)
+  }
+
+  handleSubmit = () => {
+    this.setState({input: ''})
+  }
+
+  filterResults = (search) => {
+    console.log(search)
+  }
+
   render() {
     const { shows } = this.props;
-    const { scroll } = localStyles;
+    const { scroll, textInput } = localStyles;
     const results = shows.length ? (
       shows.map(show => <ResultCard key={show.id} data={show} />)
     ) : (
       <ActivityIndicator size="large" color={accentColor} />
     );
 
-    return <ScrollView contentContainerStyle={scroll}>{results}</ScrollView>;
+    return (
+      <View>
+        <TextInput 
+          onChangeText={text => this.handleTextChange(text)}
+          onSubmitEditing={() => this.handleSubmit()}
+          style={textInput}
+          value={this.state.input} 
+          placeholder="Search Here"
+        />
+        <ScrollView contentContainerStyle={scroll}>{results}</ScrollView>
+      </View>
+    )
   }
 }
 
 const localStyles = StyleSheet.create({
   scroll: {
-    backgroundColor: secondaryColor
+    backgroundColor: "white"
+  },
+   textInput: {
+    width: "90%",
+    height: 50,
+    margin: 10,
+    backgroundColor: "white",
+    fontSize: 24,
+    padding: 8
   }
 });
 
