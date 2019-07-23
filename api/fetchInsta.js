f116aea84ed050fb07dbd9d83cc4f09eca08df5b
jacobogart <jacobogart@gmail.com>
2019-07-23 17:09:55 -0600

    Remove unused fetchInsta method

--- api/fetchInsta.js
+++
@@ -1,7 +0,0 @@
-export const fetchInsta = () => {
-  const url =
-    "https://www.instagram.com/oauth/authorize?client_id=b3445eaff7de4e8e8c29a7071e84540c&redirect_uri=http://dragme.us-east-2.elasticbeanstalk.com/auth/instagram/callback&response_type=code";
-  return fetch(url)
-    .then(response => {console.log('response', response); return response.json()})
-    .catch(error => console.log(error))
-}
