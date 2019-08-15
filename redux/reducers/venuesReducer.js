f677ef6509c95606b19eb1d11cd318b925c7c747
jacobogart <jacobogart@gmail.com>
2019-08-15 16:26:30 -0600

    Fix VenuesReducer error

--- redux/reducers/venuesReducer.js
+++ redux/reducers/venuesReducer.js
@@ -2,7 +2,7 @@ export const venuesReducer = (state = [], action) => {
   switch (action.type) {
     case "GET_VENUES":
       return action.venues;
-    case "ADD_VENUES":
+    case "ADD_VENUE":
       return [...state, action.venue];
     default:
       return state;
